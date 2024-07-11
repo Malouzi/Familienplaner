071494eddf7abd6a1a3f62db589ff47352990554
Marie Zirzlaff <marie.zirzlaff@dci-student.org>
2024-07-11 12:25:53 +0200

    Anlegen Javascript Datei Familienmitglieder

---
+++ family.js
@@ -0,0 +1,22 @@
+const colors = require('colors');
+
+const family = {
+  "Mama": {
+    color: 'red',
+    tasks: []
+  },
+  "Papa": {
+    color: 'blue',
+    tasks: []
+  },
+  "Elaine": {
+    color: 'green',
+    tasks: []
+  },
+  "Hailey": {
+    color: 'magenta',
+    tasks: []
+  }
+};
+
+module.exports = family;
